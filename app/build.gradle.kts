08c92c9916b0bc70f22c1dd526a6a46bd4de76f3
Lac Du Dinh <luuoha60@gmail.com>
2026-05-26 18:02:34 +0700

    Hoàn thiện giao diện Dashboard trang chủ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
