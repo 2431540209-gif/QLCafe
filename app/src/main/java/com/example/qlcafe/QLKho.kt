b9f820974f7ffc55fb7e19e0918e94e4d7c0dba4
quoctrieu100806-hash <quoctrieu100806@gmail.com>
2026-06-05 11:51:59 +0700

    Quản Lí Kho Hàng

---
+++ app/src/main/java/com/example/qlcafe/QLKho.kt
@@ -0,0 +1,37 @@
+package com.example.qlcafe
+
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Space
+import androidx.appcompat.app.AppCompatActivity
+
+class QLKho : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qlkho)
+
+        val layoutNhapKho =
+            findViewById<LinearLayout>(R.id.layoutNhapKho)
+
+        val spaceButton =
+            findViewById<Space>(R.id.spaceButton)
+
+        // Nhận quyền từ màn hình đăng nhập
+        val role = intent.getStringExtra("ROLE")
+
+        if (role == "staff") {
+
+            // Nhân viên: chỉ hiện Xuất kho
+            layoutNhapKho.visibility = View.GONE
+            spaceButton.visibility = View.GONE
+
+        } else {
+
+            // Quản lý: hiện cả 2 nút
+            layoutNhapKho.visibility = View.VISIBLE
+            spaceButton.visibility = View.VISIBLE
+        }
+    }
+}
